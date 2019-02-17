aaf9fb3cdc8ef63df61af83e480261e9b0893499
Dziad <18483718+Dziadzlasu@users.noreply.github.com>
2019-02-17 15:24:27 +0100

    Added and configured countries gem. Added country validation in the form.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ApplicationRecord
   validates :street, :city, presence: true
   validates :zip_code, presence: true
-  validates :country, presence: true
+  validates :country, presence: true, inclusion: { in: ISO3166::Country.all.map(&:name).concat(ISO3166::Country.all.map(&:names)).flatten }
 
   belongs_to :user, optional: true
   belongs_to :company, optional: true
