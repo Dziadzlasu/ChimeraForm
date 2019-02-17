ff98df0991169e17b9f46269f83a952a9c0aae84
Dziad <18483718+Dziadzlasu@users.noreply.github.com>
2019-02-17 13:47:42 +0100

    Changed regexp for diacritics in name validation. Updated and cleaned form view.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :first_name, :last_name, presence: true, length: { maximum: 100 }, format: /\A[a-z]+$\z/i
+  validates :first_name, :last_name, presence: true, length: { maximum: 100 }, format: /\A[\p{L}\p{M}]+\z/
   validates :email, presence: true, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   # validates :date_of_birth, allow_blank: true
   # validates :phone_number, allow_blank: true
